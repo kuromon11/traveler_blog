c44c36712baaf9d15eb606013a0019a89fb9dbe4
kuromon11 <yoshikikuro712@gmail.com>
2020-04-06 16:23:35 +0900

    image submit  temporarily

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -2,26 +2,23 @@ $(function(){
   // 画像用のinputを生成する関数
   const buildFileField = (num)=> {
     const html = `<div data-index="${num}" class="js-file_group">
-                    <label for="post_images_attributes_${num}_src">
-                      <i class="fa fa-image"></i>
-                      <input class="js-file" type="file"
-                      name="post[images_attributes][${num}][src]"
-                      id="post_images_attributes_${num}_src">
-                    </label><br>
+                    <input class="js-file" type="file"
+                    name="post[images_attributes][${num}][src]"
+                    id="post_images_attributes_${num}_src"><br>
                     <div class="js-remove">削除z</div>
                   </div>`;
     return html;
   }
-  // if ($('.js-file_group').length == 5){
+  // if ($('.js-file_group').length == 10){
   //   $('#noremove').css('display','none');
   // }
   // プレビュー用のimgタグを生成する関数
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="150px" height="100px">`;
+    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
     return html;
   }
-  // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  // file_fieldのnameに動的なindexをつける為の配列。1枚の画像が追加されるたびに最後尾に配列の値を追加し1ずつずれる。
+  let fileIndex = [1,2,3,4,5,6,7,8];
   // 既に使われているindexを除外。1番目,2番目,…と除外
   lastIndex = $('.js-file_group:last').data('index');
   // console.log(lastIndex);
@@ -31,10 +28,13 @@ $(function(){
 
 //画像を追加した時(file_fieldで画像を選択した時)
   $('#image-box').on('change', '.js-file', function(e) {
+    //imgタグが存在するかで判断するための変数
     const targetIndex = $(this).parent().data('index');
     // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
+      // 末尾の数に1足した数を追加する
+    // console.log(targetIndex);
     // imgタグが存在すれば、該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
@@ -43,11 +43,9 @@ $(function(){
       // 新規画像追加の処理
       $('#previews').append(buildImg(targetIndex, blobUrl));
       // fileIndexの先頭の数字を使ってinputを作る
-      console.log("新規");
-      // fileIndexの先頭の数字を使ってinputを作る
       $('#image-box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);      // 末尾の数に1足した数を追加する
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
       // 4こ以上増えない記述
       // if ($('.js-file').length < 4){
       //   console.log($("フォーム追加").length);
@@ -69,10 +67,10 @@ $(function(){
     //image削除
     $(`img[data-index="${targetIndex}"]`).remove();
     
-    // 画像入力欄が４個にならないようにしておく
-    if ($('.js-file').length == 1) {
-      console.log($('.js-file').length);
-      $('#forms').append(buildFileField(fileIndex[0]));
-    }
+    // // 画像入力欄が5個にならないようにしておく
+    // if ($('.js-file').length < 5) {
+    //   console.log($('.js-file').length);
+    //   $('#forms').append(buildFileField(fileIndex[0]));
+    // }
   });
 });
