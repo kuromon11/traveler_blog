f648792d6a3268491922a5c2bdf94d7ee77da288
kuromon11 <yoshikikuro712@gmail.com>
2020-04-07 10:28:26 +0900

    add colomn users

---
+++ db/migrate/20200407011551_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :image, :string
+    add_column :users, :information, :text
+    add_column :users, :gender_id, :integer, null: false
+    add_column :users, :residence_id, :integer, null: false
+  end
+end
