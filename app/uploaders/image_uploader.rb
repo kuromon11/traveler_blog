5437ac6a57c965a5035c21f4a29547ffeecc1808
kuromon11 <yoshikikuro712@gmail.com>
2020-04-26 14:52:40 +0900

    process :convert => 'jpg'

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,6 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   # version :thumb do
     process resize_to_fit: [400, 400]
+    process :convert => 'jpg'
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
